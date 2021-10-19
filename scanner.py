de83b558bbd9b2c1df8d04020c70f58549950fe6
Barzu Andrei-Daniel <andrewbarzu@gmail.com>
2021-10-19 19:15:31 +0300

    Improved scanner to use a regex for splitting the file into tokens

--- scanner.py
+++ scanner.py
@@ -1,7 +1,9 @@
+import re
 from typing import Tuple, List, Union
 
 from hashmap import Hashmap as SymbolTable
 import sys
+from functools import reduce
 
 class LexicalError(Exception):
     pass
@@ -24,24 +26,55 @@ reserved_words = {
     "void": 40
 }
 
+escaped_operators = {
+    "\\+\\+": 28,
+    "--": 29,
+    "\\+": 14,
+    "-": 15,
+    "//": 25,
+    "/": 16,
+    "\\*": 17,
+    "<=": 19,
+    ">=": 24,
+    "==": 21,
+    "!=": 22,
+    "=": 18,
+    "<": 20,
+    ">": 23,
+    "&&": 26,
+    "\\|\\|": 27,
+    "%": 30,
+}
+
 operators = {
     "+": 14,
     "-": 15,
     "/": 16,
     "*": 17,
-    "=": 18,
     "<=": 19,
-    "<": 20,
+    ">=": 24,
     "==": 21,
     "!=": 22,
+    "=": 18,
+    "<": 20,
     ">": 23,
-    ">=": 24,
     "//": 25,
     "&&": 26,
     "||": 27,
     "++": 28,
     "--": 29,
     "%": 30,
+}
+
+escaped_separators = {
+    "\\[": 31,
+    "\\]": 32,
+    "\\{": 33,
+    "\\}": 34,
+    "\\(": 35,
+    "\\)": 36,
+    ";": 38,
+    ",": 39,
 }
 
 separators = {
@@ -59,7 +92,7 @@ def isIdentifier(token: str) -> bool:
     return len(token) >= 1 and token[0].isalpha() and (len(token) == 1 or token[1:].isalnum())
 
 def isNumber(token: str) -> bool:
-    return len(token) > 1 and (token.isnumeric() or (token[0] in ("+", "-") and token[1:].isnumeric()))
+    return len(token) >= 1 and (token.isnumeric() or (token[0] in ("+", "-") and token[1:].isnumeric()))
 
 def isChar(token: str) -> bool:
     return len(token) == 3 and token[0] == "'" and token[-1] == "'"
@@ -76,7 +109,7 @@ def isConstant(token: str) -> bool:
 def detect(token: str, reserved_words: dict) -> str:
     if token == "":
         return "NONE"
-    if token in reserved_words:
+    if token in reserved_words or token in separators or token in operators:
         return token
     if isIdentifier(token):
         return "IDENTIFIER"
@@ -84,74 +117,35 @@ def detect(token: str, reserved_words: dict) -> str:
         return "CONSTANT"
     raise LexicalError("Token `{0}` is not a reserved word or a valid identifier or constant!".format(token))
 
-
-def scan(filename: str, reserved_words: dict, operators: dict, separators: dict) -> Tuple[SymbolTable, List]:
+def scan(filename: str, separator_pattern: re.Pattern, reserved_words: dict) -> Tuple[SymbolTable, List]:
     ST = SymbolTable()
     PIF: List[Tuple[str, Union[Tuple[int, int], 0]]] = list()
-    word = ""
     lineNumber = 0
     with open(filename) as f:
         for line in f:
             lineNumber += 1
             i = 0
-            while i < len(line):
-                if line[i] in (' ', '\n'):
-                    try:
-                        i += 1
-                        tok = detect(word, reserved_words)
-                        if tok == "NONE":
-                            continue
-                        pos = 0
-                        if tok in ("IDENTIFIER", "CONSTANT"):
-                            pos = ST.add(word)
-                        PIF.append((tok, pos))
-                    except LexicalError as e:
-                        print(str(e) + " on line {0}, column {1}".format(lineNumber, i - len(word)))
-                    word = ""
+            for token in separator_pattern.split(line):
+                if token == " " or token == "\n" or token == "":
                     continue
-
-                if line[i] in separators.keys() or line[i] in operators.keys():
-                    try:
-                        tok = detect(word, reserved_words)
-                        if tok == "NONE":
-                            PIF.append((line[i], 0))
-                            i += 1
-                            continue
-                        pos = 0
-                        if tok in ("IDENTIFIER", "CONSTANT"):
-                            pos = ST.add(word)
-                        PIF.append((tok, pos))
-                    except LexicalError as e:
-                        print(str(e) + " on line {0}, column {1}".format(lineNumber, i - len(word)))
-                    PIF.append((line[i], 0))
-                    word = ""
-                    i += 1
-
-                elif line[i: i + 2] in operators.keys():
-                    try:
-                        tok = detect(word, reserved_words)
-                        if tok == "NONE":
-                            PIF.append((line[i: i+2], 0))
-                            i += 2
-                            continue
-                        pos = 0
-                        if tok in ("IDENTIFIER", "CONSTANT"):
-                            pos = ST.add(word)
-                        PIF.append((tok, pos))
-                    except LexicalError as e:
-                        print(str(e) + " on line {0}, column {1}".format(lineNumber, i - len(word)))
-                    PIF.append((line[i: i + 2], 0))
-                    word = ""
-                    i += 2
-
-                else:
-                    word += line[i]
-                    i += 1
+                i += 1
+                try:
+                    tok = detect(token, reserved_words)
+                    pos = 0
+                    if tok in ("IDENTIFIER", "CONSTANT"):
+                        pos = ST.add(token)
+                    PIF.append((tok, pos))
+                except LexicalError as e:
+                    print(str(e) + " on line {0}, token {1}".format(lineNumber, i))
 
     return ST, PIF
 
 if __name__ == "__main__":
     filename = sys.argv[1]
-    ST, PIF = scan(filename, reserved_words, operators, separators)
+    reducer = lambda x, y: x + "|" + y
+    regex_separators = r'(' + str(reduce(reducer, escaped_operators.keys())) + "|" + str(reduce(reducer, escaped_separators.keys())) + '| |\\r\\n)'
+    print(regex_separators)
+    regex_separator_pattern = re.compile(regex_separators)
+    ST, PIF = scan(filename, regex_separator_pattern, reserved_words)
     print(str(ST))
     print(PIF)
